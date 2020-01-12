a1251e99433d83656371e6a23869f5b54313d53b
Kzero <tennoken@naver.com>
2020-01-13 02:21:18 +0900

    Add Detail

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,17 +1,26 @@
 import React from "react";
+import "./Detail.css";
+
 
 class Detail extends React.Component {
     componentDidMount(){
         const { location, history } = this.props;
-        console.log(location.state);
         if(location.state === undefined){
             history.push("/");
         }
     }
     render(){
-        const {location} = this.props
+        const {location} = this.props;
         if(location.state){
-            return <span>{location.state.title}</span>;
+            return (
+            <div className="detail">
+                <img src={location.state.poster} alt=""/>
+                <h1>{location.state.title}</h1>
+                <h3>{location.state.year}</h3>
+                <h4>{location.state.genres.join(' ')}</h4>
+                {location.state.summary}
+            </div>
+            );
         }else {
             return null;
         }
